4baa0f059b6c13bdbb4099d32eab6168d3987c3c
Andres Ortiz <candresortizm@gmail.com>
2016-11-02 21:56:28 -0500

    consulta datos de la BD

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161102024504) do
+ActiveRecord::Schema.define(version: 20161103022014) do
 
-  create_table "users", force: :cascade do |t|
+  create_table "chirps", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "body"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "email"
     t.string   "first_name"
     t.string   "last_name"
